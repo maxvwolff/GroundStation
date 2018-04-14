5d0e189afe70d28404d6f572f073b8c78589420d
maxvonwolff <max@maxvw.de>
2018-04-14 16:35:32 +0200

    Updated

--- Application/web/flaskr.py
+++ Application/web/flaskr.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/control')
-def static_page(page_name):
+def static_page():
     return render_template('index.html')
 
 if __name__ == '__main__':
